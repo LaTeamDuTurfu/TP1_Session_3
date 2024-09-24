c4dcfda63b5574b1e56d810b08f8b43ee48f9b6d
Joe1651 <jonathan.duplantis@hotmail.com>
2024-09-24 14:36:21 -0400

    modifs classe joueur

--- Controllers/catalogue.py
+++ Controllers/catalogue.py
@@ -9,7 +9,7 @@ class Catalogue:
 
     def ajouter_partie(self):
         # Heading
-        print("------ Ajouter une partie -----")
+        print("------ Ajouter une partie -----\n")
 
         # Création des joueurs
         joueur1_nom = lire_chaine("Entrez le nom du joueur 1: ")
@@ -19,7 +19,15 @@ class Catalogue:
         joueur1 = Joueur(joueur1_nom, joueur1_niveau)
         joueur2 = Joueur(joueur2_nom, joueur2_niveau)
 
-        #
+        # Spécifitiés de la partie
+        date = lire_date("Entrez la date à laquelle la date a été jouée (DD-MM-AAAA): ")
+        durée = lire_entier("Entrez la durée de la partie en minutes: ")
+        gagnant = lire_entier_intervalle(f"Est-ce {joueur1_nom} [1] qui à gagné ou {joueur2_nom} [2]: ")
+        ouverture = lire_caractère("Quelle était l'ouverture utilisée: ")
+        moves = input("Écrivez les moves dans un format PGN: ")
+
+        # Création de la partie
+        nouvelle_partie = Partie(joueur1)
 
     def modifier_partie(self):
         pass
--- Models/partie.py
+++ Models/partie.py
@@ -6,8 +6,6 @@ class Partie:
         self.joueur1: Joueur = joueur1
         self.joueur2: Joueur = joueur2
         self.date = date  # Format DD/MM/AAAA
-        self.niveau_j1: int = niveau_j1
-        self.niveau_j2: int = niveau_j2
         self.type_partie: str = type_partie
         self.durée: int = durée  # Durée en minutes
         self.résultat = résultat
