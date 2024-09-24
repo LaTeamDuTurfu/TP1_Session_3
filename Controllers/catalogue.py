365246373ed5304b127c7e3e3fb4d003804d802b
Joe1651 <jonathan.duplantis@hotmail.com>
2024-09-24 14:17:08 -0400

    Caca

--- Controllers/catalogue.py
+++ Controllers/catalogue.py
@@ -1,4 +1,6 @@
 from Controllers import Database
+from Models import *
+from ModuleUtilitaire.utilpy.Console.console_utils import *
 
 
 class Catalogue:
@@ -6,7 +8,18 @@ class Catalogue:
         self.database = database
 
     def ajouter_partie(self):
-        pass
+        # Heading
+        print("------ Ajouter une partie -----")
+
+        # Création des joueurs
+        joueur1_nom = lire_chaine("Entrez le nom du joueur 1: ")
+        joueur1_niveau = lire_entier("Entrez l'Elo du joueur 1:")
+        joueur2_nom = lire_chaine("Entrez le nom du joueur 2: ")
+        joueur2_niveau = lire_entier("Entrez l'Elo du joueur 2:")
+        joueur1 = Joueur(joueur1_nom, joueur1_niveau)
+        joueur2 = Joueur(joueur2_nom, joueur2_niveau)
+
+        #
 
     def modifier_partie(self):
         pass
--- Models/partie.py
+++ Models/partie.py
@@ -2,7 +2,7 @@ from Models import Joueur
 
 
 class Partie:
-    def __init__(self, joueur1, joueur2, date, niveau_j1, niveau_j2, type_partie, durée, résultat, ouverture):
+    def __init__(self, joueur1, joueur2, date, niveau_j1, niveau_j2, type_partie, durée, résultat, ouverture, moves):
         self.joueur1: Joueur = joueur1
         self.joueur2: Joueur = joueur2
         self.date = date  # Format DD/MM/AAAA
@@ -12,6 +12,7 @@ class Partie:
         self.durée: int = durée  # Durée en minutes
         self.résultat = résultat
         self.ouverture: str = ouverture
+        self.moves: str = moves
 
     def __str__(self):
         return f"{self.joueur1} vs {self.joueur2} | {self.type_partie} | {self.date}"
