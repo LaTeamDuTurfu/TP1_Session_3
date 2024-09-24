772ee6c82686c48c8819fc4f0ade89b03bde4c94
Joe1651 <jonathan.duplantis@hotmail.com>
2024-09-24 13:48:31 -0400

    Typage des données de la classe partie.py

--- Models/partie.py
+++ Models/partie.py
@@ -1,14 +1,14 @@
 class Partie:
     def __init__(self, joueur1, joueur2, date, niveau_j1, niveau_j2, type_partie, durée, résultat, ouverture):
-        self.joueur1 = joueur1
-        self.joueur2 = joueur2
-        self.date = date
-        self.niveau_j1 = niveau_j1
-        self.niveau_j2 = niveau_j2
-        self.type_partie = type_partie
-        self.durée = durée
+        self.joueur1: str = joueur1
+        self.joueur2: str = joueur2
+        self.date = date  # Format DD/MM/AAAA
+        self.niveau_j1: int = niveau_j1
+        self.niveau_j2: int = niveau_j2
+        self.type_partie: str = type_partie
+        self.durée: int = durée  # Durée en minutes
         self.résultat = résultat
-        self.ouverture = ouverture
+        self.ouverture: str = ouverture
 
     def __str__(self):
         return f"{self.joueur1} vs {self.joueur2} | {self.type_partie} | {self.date}"
