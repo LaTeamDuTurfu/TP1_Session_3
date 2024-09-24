7f35d6b9086cab7b4d438ba8e1c31cd2f4d4289e
ZCloutier <2370266@cstjean.qc.ca>
2024-09-24 15:01:46 -0400

    db

--- Controllers/database.py
+++ Controllers/database.py
@@ -1,9 +1,29 @@
+from Models import *
+import os
+
 class Database:
     def __init__(self):
         self.parties = []
+        self.folder_path = 'Data'
 
     def load_parties(self):
-        pass
+        for file in os.listdir(self.folder_path):
+            if file.endswith('.pgn'):
+                file_path = os.path.join(self.folder_path, file)
+
+                with open(file_path, 'r') as f:
+                    nouvelle_partie = Partie()
+                    nouvelle_partie.joueur1 = f.readline()
+                    nouvelle_partie.joueur2 = f.readline()
+                    nouvelle_partie.date = f.readline()
+                    nouvelle_partie.type_partie = f.readline()
+                    nouvelle_partie.durée = f.readline()
+                    nouvelle_partie.résultat = f.readline()
+                    nouvelle_partie.ouverture = f.readline()
+                    nouvelle_partie.moves = f.readline()
+
 
     def save_parties(self):
         pass
+
+
